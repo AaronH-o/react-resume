853143f908c4b19195af2b8bd25815a263e5a458
Aaron Ho <bicecreamaar@gmail.com>
2024-09-04 21:53:22 -0700

    updated file names and modified experience page

--- src/pages/ExperiencePage.jsx
+++ src/pages/ExperiencePage.jsx
@@ -3,13 +3,7 @@ export default function AboutPage() {
   return (
     <div className="container pt-4">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-        impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-        architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-        mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-        nostrum temporibus ad omnis nam rerum eligendi.
+        What should I put here
       </p>
       <section className="features-icons bg-light text-center m-4">
         <div className="container">
@@ -51,13 +45,7 @@ export default function AboutPage() {
         </div>
       </section>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-        impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-        architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-        mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-        nostrum temporibus ad omnis nam rerum eligendi.
+        not that much experience :( but I own a dog!
       </p>
     </div>
   );
