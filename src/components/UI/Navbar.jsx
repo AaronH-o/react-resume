873e3fd51a8d4c70d615fea026f56f318933f7df
Aaron Ho <bicecreamaar@gmail.com>
2024-09-24 18:15:40 -0700

    made the icon redirect back to the homepage

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/heart.svg';
-import Resume from '../../assets/Aaron_Ho_Resume.pdf';
 
 export default function Nav({ links }) {
   return (
     <nav className="navbar navbar-expand-lg bg-secondary">
       <div className="container-fluid">
-        <img src={Logo} alt="Logo" width="50" height="40" class="d-inline-block align-text-top"/>
+        <Link to="/">
+          <img src={Logo} alt="Logo" width="50" height="40" class="d-inline-block align-text-top"/>
+        </Link>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             {links.map((link) => link)}
