b71f1b18e326db2657633d5b453baea168b2c824
Aaron Ho <bicecreamaar@gmail.com>
2025-03-21 15:33:31 -0700

    resume now opens an additional tab

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,9 +15,9 @@ export default function Nav() {
           Projects
         </Link>,
         // TODO: change to button and add styling for tooltip on the bottom
-        <a key={3} className="nav-link text-light" href={Resume}>
+        <Link key={3} className="nav-link text-light" to={Resume} target="_blank" rel="noopener noreferrer">
           Resume
-        </a>,
+        </Link>,
       ]}
     />
   );
